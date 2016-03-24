12aefa61c0b7fdde66d71ef1427ef6150e85bfde
SayChunKim <saychunkim@gmail.com>
2016-03-24 16:46:47 +0800

    First Commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var sidenav = new Sidenav({
+  content: document.getElementById("content"),
+  sidenav: document.getElementById("sidenav"),
+  backdrop: document.getElementById("backdrop")
+});
+
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  sidenav.open();
+}, true);
